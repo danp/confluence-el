e0b5e377466e014a99ff94e0b6e0179a6fddd51a
kyle.burton <kyle.burton@ba13cf60-b194-11dd-9e4a-13528392d412>
2008-11-19 18:21:41 +0000

    adding more docs
    git-svn-id: http://confluence-el.googlecode.com/svn/trunk@39 ba13cf60-b194-11dd-9e4a-13528392d412

--- confluence.el
+++ confluence.el
@@ -57,7 +57,100 @@
 ;; To view the chagnes in your page versus what is in the wiki, type
 ;; \C-xw=, or run M-x confluence-ediff-current-page.
 ;;
-
+;; (add-hook 'confluence-mode-hook
+;;   (local-set-key "\C-xw" confluence-prefix-map)
+;;   (local-set-key [(control f6)] 'confluence-ediff-merge-current-page)
+;;   (local-set-key "\C-j" 'confluence-newline-and-indent))
+;;
+;; LONGLINES
+;;
+;;   http://www.emacswiki.org/emacs-en/LongLines
+;;
+;; Confluence uses a wiki-markup that treats linebreask as <p> HTML
+;; tags.  Since this is the case, it is very common for paragraphs in
+;; the Confluence markup to wrap around your buffer multiple times.
+;; The LongLines mode allows those lines to be viewed within Emacs
+;; with 'soft' linebreaks - which are inserted automatically, or via
+;; M-q.  This makes it much more pleasant to work with large
+;; paragraphs of text in the Confluence markup without introducing
+;; unwated paragraphs.
+;;
+;; See below for more advice on using LongLines and confluence-mode.
+;;
+;;
+;; EXAMPLE .emacs CONFIGURATION
+;;
+;; (load (expand-file-name "~/software/emacs/confluence-el/xml-rpc.el"))
+;; (load (expand-file-name "~/software/emacs/confluence-el/confluence.el"))
+;;
+;; ;; note, all customization must be in *one* custom-set-variables block
+;; (custom-set-variables
+;;  ;; ... other custimization
+;;
+;;  ;; confluence customization
+;;  '(confluence-url "http://intranet/confluence/rpc/xmlrpc")
+;;  '(confluence-default-space-alist (list (cons confluence-url "your-default-space-name"))))
+;;
+;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; ;; confluence editing support (with longlines mode)
+;;
+;; (autoload 'confluence-get-page "confluence" nil t)
+;;
+;; (eval-after-load "confluence"
+;;   '(progn
+;;      (require 'longlines)
+;;      (progn
+;;        (add-hook 'confluence-mode-hook 'longlines-mode)
+;;        (add-hook 'confluence-before-save-hook 'longlines-before-revert-hook)
+;;        (add-hook 'confluence-before-revert-hook 'longlines-before-revert-hook))))
+;;
+;; ;; LongLines mode: http://www.emacswiki.org/emacs-en/LongLines
+;; (autoload 'longlines-mode "longlines" "LongLines Mode." t)
+;;
+;; (eval-after-load "longlines"
+;;   '(progn
+;;      (defvar longlines-mode-was-active nil)
+;;      (make-variable-buffer-local 'longlines-mode-was-active)
+;;
+;;      (defun longlines-suspend ()
+;;        (if longlines-mode
+;;            (progn
+;;              (setq longlines-mode-was-active t)
+;;              (longlines-mode 0))))
+;;
+;;      (defun longlines-restore ()
+;;        (if longlines-mode-was-active
+;;            (progn
+;;              (setq longlines-mode-was-active nil)
+;;              (longlines-mode 1))))
+;;
+;;      ;; longlines doesn't play well with ediff, so suspend it during diffs
+;;      (defadvice ediff-make-temp-file (before make-temp-file-suspend-ll
+;;                                              activate compile preactivate)
+;;        "Suspend longlines when running ediff."
+;;        (with-current-buffer (ad-get-arg 0)
+;;          (longlines-suspend)))
+;;
+;;     
+;;      (add-hook 'ediff-cleanup-hook 
+;;                '(lambda ()
+;;                   (dolist (tmp-buf (list ediff-buffer-A
+;;                                          ediff-buffer-B
+;;                                          ediff-buffer-C))
+;;                     (if (buffer-live-p tmp-buf)
+;;                         (with-current-buffer tmp-buf
+;;                           (longlines-restore))))))))
+;;
+;; ;; keybindings (change to suit)
+;;
+;; ;; open confluence page
+;; (global-set-key "\C-xwf" 'confluence-get-page)
+;;
+;; ;; setup confluence mode
+;; (add-hook 'confluence-mode-hook
+;;           '(lambda ()
+;;              (local-set-key "\C-xw" confluence-prefix-map)))
+;;
 ;; 
 
 ;;; Code:
